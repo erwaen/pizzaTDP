3fc76f4f0aa3e7bf186553c2f4f7d21342e30c83
erwaen <erikwasmosy98@gmail.com>
2022-07-01 18:16:42 -0400

    Archivo de configuracion del motor de base de dato

---
+++ apiPizza/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# El URL del archivo de la base de datos (./pizza_app.db)
+SQLALCHEMY_DATABASE_URL = "sqlite:///./pizza_app.db"
+
+# Se crea un SQLAlchemy 'engine'
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+# Cada instancia de esta clase ("SessionLocal") sera una sesion de la base de datos.
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+# Esta clase 'Base' se utilizara para heredar de el los modelos a crear para
+# la base de datos
+Base = declarative_base()
