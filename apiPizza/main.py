0cf32131e17931487b6789e1f4ca23da6edf6616
erwaen <erikwasmosy98@gmail.com>
2022-07-01 20:07:14 -0400

    creacion de path/endpoint para users

--- apiPizza/main.py
+++ apiPizza/main.py
@@ -1,8 +1,44 @@
-from fastapi import FastAPI
+from typing import List
+
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+from . import crud, modelos, schemas
+from .database import SessionLocal, engine
+
+modelos.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_username(db, username=user.username)
+    if db_user: # Si el usuario ya existe
+        raise HTTPException(status_code=400, detail="Ya se creo un user con este username")
+    return crud.create_user(db=db, user=user)
+
+
+@app.get("/users/", response_model=List[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None: # Si el usuario no se encontro
+        raise HTTPException(status_code=404, detail="Usuario no encontrado")
+    return db_user
